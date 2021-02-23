d3a206e4f05da7f00a9bf16803824494df1122c1
Daniel Eisterhold <deisterhold@gmail.com>
2021-02-22 22:00:20 -0600

    Add code to wait for drive to be ready.

--- ripper/src/server.ts
+++ ripper/src/server.ts
@@ -1,111 +1,122 @@
 import { exec, spawn } from 'child_process';
-import { platform } from 'os';
+import { stat } from 'fs';
 
-let udev: any;
-if (platform() == 'linux'){
-    udev = require('udev');
+enum DriveStatus {
+    Open = 'drive_open',
+    Loading = 'drive_not_ready',
+    Ready = 'drive_ready',
+    Empty = 'drive_empty'
 }
 
-function getDriveStatus(device: string = null) {
-    const OPEN = 'drive_open';
-    const EMPTY = 'drive_empty';
-    const LOADING = 'drive_not_ready';
-    const READY = 'drive_ready';
-
-    return new Promise(function(resolve, reject) {
-        exec(`setcd -i ${device || '/dev/sr0'}`, function(err, stdout, stderr) {
+function getDriveStatus(device: string = null): Promise<DriveStatus> {
+    return new Promise<DriveStatus>(function (resolve, reject) {
+        exec(`setcd -i ${device || '/dev/sr0'}`, function (err, stdout, stderr) {
             if (err) {
                 console.error(stderr);
                 reject(err);
             }
-            
-            stdout = stdout || '';
 
-            if (stdout.indexOf('CD tray is open') !== -1){
-                resolve(OPEN);
-            } else if (stdout.indexOf('Drive is not ready') !== -1){
-                resolve(LOADING);
-            } else if (stdout.indexOf('Disc found in drive') !== -1){
-                resolve(READY);
-            } else if (stdout.indexOf('No disc is inserted') !== -1){
-                resolve(EMPTY);
+            status = stdout || '';
+
+            if (status.indexOf('CD tray is open') !== -1) {
+                resolve(DriveStatus.Open);
+            } else if (status.indexOf('Drive is not ready') !== -1) {
+                resolve(DriveStatus.Loading);
+            } else if (status.indexOf('Disc found in drive') !== -1) {
+                resolve(DriveStatus.Ready);
+            } else if (status.indexOf('No disc is inserted') !== -1) {
+                resolve(DriveStatus.Empty);
             } else {
-                resolve(null);
+                reject(new Error(`Unrecognized drive status: ${status}`));
             }
-
-            resolve(stdout);
         });
     });
 }
 
 function getVolumeId() {
-    return new Promise(function(resolve, reject) {
-        exec('isoinfo -d -i /dev/sr0 | grep "Volume id"', function(err, stdout, stderr) {
+    return new Promise(function (resolve, reject) {
+        exec('isoinfo -d -i /dev/sr0 | grep "Volume id"', function (err, stdout, stderr) {
             if (err) {
                 console.error(stderr);
                 reject(err);
             }
 
-            resolve(stdout);
+            var name = (stdout || '').replace('Volume id: ', '').trim();
+
+            resolve(name);
         });
     });
 }
 
-function createIso() {
-    // Call dd command
-    const dd = spawn('/bin/dd', ['if=', 'of']);
-    // If dd doesn't return data in 10s, kill it
-    const timeout = setTimeout(function(){
-        console.log('Command timed out...killing.');
-        dd.kill();
-    }, 10000);
+// function createIso(outputPath: string) {
+//     // Call dd command
+//     const dd = spawn('/bin/dd', ['if=', 'of']);
+//     // If dd doesn't return data in 10s, kill it
+//     const timeout = setTimeout(function(){
+//         console.log('Command timed out...killing.');
+//         dd.kill();
+//     }, 10000);
 
-    dd.stdout.on('data', data => {
-        clearTimeout(timeout);
-        console.log(`stdout: ${data}`);
-    });
-    
-    dd.stderr.on('data', data => {
-        clearTimeout(timeout);
-        console.log(`stderr: ${data}`);
-    });
-    
-    dd.on('error', (error) => {
-        clearTimeout(timeout);
-        console.log(`error: ${error.message}`);
-    });
-    
-    dd.on('close', code => {
-        clearTimeout(timeout);
-        console.log(`child process exited with code ${code}`);
-    });
+//     dd.stdout.on('data', data => {
+//         clearTimeout(timeout);
+//         console.log(`stdout: ${data}`);
+//     });
 
-    return dd;
-};
+//     dd.stderr.on('data', data => {
+//         clearTimeout(timeout);
+//         console.log(`stderr: ${data}`);
+//     });
+
+//     dd.on('error', (error) => {
+//         clearTimeout(timeout);
+//         console.log(`error: ${error.message}`);
+//     });
+
+//     dd.on('close', code => {
+//         clearTimeout(timeout);
+//         console.log(`child process exited with code ${code}`);
+//     });
+
+//     return dd;
+// };
 
 // var volumeName = getVolumeId();
 
 // volumeName.then(console.log, console.error);
 
 // Drive status
-var driveCheck = setInterval(function () {
-    var driveStat = getDriveStatus();
 
-    driveStat.then(console.log, console.error);
-}, 30000);
+function waitForDrive(): Promise<void> {
+    const delay = 5000;
 
-// Monitor events on the scis subsystem until a device is added.
-var monitor = udev.monitor("scis");
+    return new Promise(function (resolve, reject) {
+        let running = false;
+        var interval = setInterval(function () {
+            if (running) return;
 
-monitor.on('add', function(device: any) {
-    console.log('added ' + device);
-    monitor.close() // this closes the monitor
-});
+            running = true;
+            getDriveStatus()
+                .then((status) => {
+                    if (status == DriveStatus.Ready) {
+                        clearInterval(interval);
+                        resolve();
+                    }
+                })
+                .catch(reject)
+                .finally(() => {
+                    running = false;
+                });
+        }, delay);
+    });
+}
 
-monitor.on('remove', function (device: any) {
-    console.log('removed ' + device);
-});
+async function process(): Promise<void> {
+    var exit = false;
 
-monitor.on('change', function (device: any) {
-    console.log('changed ' + device);
-});
+    while (!exit) {
+        await waitForDrive();
+        console.log('Drive is ready for ripping.');
+    }
+}
+
+process();
